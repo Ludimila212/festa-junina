f76d16ad1abfaab53b570f951125ee725df91506
Ludimila212 <malikmilla54@gmail.com>
2024-06-25 08:46:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ from modelos.jurado import Jurado
 from modelos.aluno import Aluno
 
 rhaenys = Aluno('Rhaenys', 'TI', 'Miss')
+laena = Aluno('Laena', 'TI', 'Miss')
+romina = Aluno('Romina', 'TI', 'Miss')
+thiago = Aluno('Thiago', 'TI', 'Mister')
+
 jurado = Jurado('Daemon', 'Educacional')
+jurado = Jurado('Lucerys', 'Educacional')
+jurado = Jurado('Rhaenrya', 'Educacional')
 
 jurado.atribuir_nota(rhaenys, 5, 5, 5, 5)
-jurado.atribuir_nota(rhaenys, 6, 6, 6, 6)
+jurado.atribuir_nota(laena, 6, 6, 6, 6)
+jurado.atribuir_nota(romina,10,10,10,10)
+jurado.atribuir_nota(thiago, 5,3,2,7)
 
 print(rhaenys)
+print(laena)
+print(romina)
+print(thiago)
 
 def main():
     pass
